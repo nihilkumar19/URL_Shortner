7c81f6d829dafddd3b94f61b7df60c46d13c50ac
Nihil Kumar <76404574+nihilkumar19@users.noreply.github.com>
2023-02-11 13:09:25 +0530

    Add files via upload

---
+++ URLSHORTING.py
@@ -0,0 +1,17 @@
+import requests
+
+api_key = "520a231aa2ec5caa926492e1b336e89ca9ea1"
+# the URL you want to shorten
+url = "https://www.thepythoncode.com/topic/using-apis-in-python"
+# preferred name in the URL
+api_url = f"https://cutt.ly/api/api.php?key={api_key}&short={url}"
+# or
+# api_url = f"https://cutt.ly/api/api.php?key={api_key}&short={url}&name=some_unique_name"
+# make the request
+data = requests.get(api_url).json()["url"]
+if data["status"] == 7:
+    # OK, get shortened URL
+    shortened_url = data["shortLink"]
+    print("Shortened URL:", shortened_url)
+else:
+    print("[!] Error Shortening URL:", data)
